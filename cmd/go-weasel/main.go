7ad53dfa8a1d229bd666026f70df310c2e019537
eror202 <yura211252@gmail.com>
2024-02-24 20:19:10 +0300

    Add generation counting

--- cmd/go-weasel/main.go
+++ cmd/go-weasel/main.go
@@ -13,12 +13,18 @@ func main() {
 
 	sample := "METHINKS IT IS LIKE A WEASEL"
 	current := weasel.Initialize(len(sample))
+	var generationCouner int
 	for current != sample {
-		fmt.Println(current)
+		const outputRate = 10
+		if generationCouner%outputRate == 0 {
+			fmt.Println(generationCouner, current)
+		}
 
 		variants := weasel.Populate(current, 0.05, 100)
 		current = weasel.Search(variants, sample)
+
+		generationCouner++
 	}
 
-	fmt.Println(current)
+	fmt.Println(generationCouner, current)
 }
