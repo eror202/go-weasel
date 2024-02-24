91a8b4463733f9a9f5ca7d833e22bf916ce90f32
eror202 <yura211252@gmail.com>
2024-02-24 18:59:50 +0300

    Add the Populate function

---
+++ populate_test.go
@@ -0,0 +1,23 @@
+package weasel
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+)
+
+func TestPopulate(test *testing.T) {
+	rand.Seed(1)
+
+	text := "the quick brown fox jumps over the lazy dog"
+	actualResult := Populate(text, 0.2, 3)
+
+	expectedResult := []string{
+		"the qu Pk brown fox jumps oveF tGD Nazy dog",
+		"the N ick broRn foS jumps oveB theTlazyEdQg",
+		"the quWck b Ewn foxMjumNs Qver She lRzy dog",
+	}
+	if !reflect.DeepEqual(actualResult, expectedResult) {
+		test.Fail()
+	}
+}
